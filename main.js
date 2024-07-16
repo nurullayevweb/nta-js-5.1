e3e9a93b438856fb7c6c47f0b4d2e60acd875e32
nurullayevweb <nurullayevweb@gmail.com>
2024-07-16 11:12:18 +0500

    fizzbuzz function final

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 var elText = document.querySelector(".js-text");
 var elErrorText = document.querySelector(".js-error-text");
-var numberbuzz=Number(prompt("Fizzbuz o'yini uchun son kiriting"))
-var a = 3;
-var b = 5;
 
-if (numberbuzz < a) {
-   elErrorText.textContent = "Eng kamida 3 sonini kiriting";
-} else if (numberbuzz % a == 0 && numberbuzz % b == 0) {
-   elText.textContent = "FIZZBUZZ";
-} else if (numberbuzz % a == 0) {
-   elText.textContent = "FIZZ";
-} else if (numberbuzz % b == 0 ) {
-   elText.textContent = "BUZZ";
-} else {
-   elErrorText.textContent = `${numberbuzz} soni 3 ga ham 5 ga ham bo'linmaydi`;
-};
+function checkFizzBuzz(num) {
+   if (num < 3) {
+      return elErrorText.textContent = "Eng kamida 3 sonini kiriting";
+   }
+   if (num % 3 == 0 && num % 5 == 0) {
+      return elText.textContent = "FIZZBUZZ";
+   }
+   if (num % 3 == 0) {
+      return elText.textContent = "FIZZ";
+   }
+   if (num % 5 == 0) {
+      return elText.textContent = "BUZZ";
+   }
+   else {
+      return elErrorText.textContent = `${num} soni 3 ga ham 5 ga ham bo'linmaydi`;
+   };
+}
+
+var numberbuzz = Number(prompt("Fizzbuz o'yini uchun son kiriting"))
+checkFizzBuzz(numberbuzz)
